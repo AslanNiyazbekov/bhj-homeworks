6b4388cf6a42df0b04e07299902cab23c95ea251
AslanNiyazbekov <tanside@yandex.ru>
2022-09-12 20:01:39 +0500

    «Способы поиска нужного HTML-элемента»2

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -6,9 +6,7 @@ const sliderArrowprev = document.querySelector('.slider__arrow_prev');
 const arr = Array.from(sliderItem);
 
 sliderArrowprev.onclick = function () {
-    i = arr.findIndex((element) => {
-        return element.classList.contains('slider__item_active')
-    });
+    i = arr.findIndex((element) => element.classList.contains('slider__item_active'));
     arr[i].classList.remove('slider__item_active');
     i = (i > 0) ? i - 1 : arr.length - 1;
     arr[i].classList.add('slider__item_active');
